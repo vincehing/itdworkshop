3542aab55602617beb650cce76253149b0087d2e
lthben <Lthben@gmail.com>
2023-07-31 12:54:59 +0800

    commit

--- class.py
+++ class.py
@@ -48,11 +48,11 @@ ch10 = "Challenge 10"
 final = "Yoda Chatbot"
 
 st.set_page_config(layout="wide")
+st.session_state.login_key = True
 
 def main():
 	st.title("ITD Sharing Workshops 2023")
 
-	st.session_state.login_key = True
 	
 	if 'login_key' not in st.session_state:
 		st.session_state.login_key = False
