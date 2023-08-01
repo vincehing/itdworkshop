5155c72486c6c951e3066e73b816865e8ec05d14
lthben <Lthben@gmail.com>
2023-08-01 11:58:41 +0800

    c

--- class1_exercise.py
+++ class1_exercise.py
@@ -757,9 +757,9 @@ def class1_ch6():
 		st.session_state.messages = []
 
 	# # Display chat messages from history on app rerun
-	# for message in st.session_state.messages:
-	# 	with st.chat_message(message["role"]):
-	# 		st.markdown(message["content"])
+	for message in st.session_state.messages:
+		with st.chat_message(message["role"]):
+			st.markdown(message["content"])
 
 	# React to user input
 	if prompt := st.chat_input("Enter your query"):
