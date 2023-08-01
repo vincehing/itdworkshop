bea8d391aa5b158d1b0c1ec4bbf884ace9a7f546
lthben <Lthben@gmail.com>
2023-08-01 11:53:39 +0800

    c

--- class1_exercise.py
+++ class1_exercise.py
@@ -689,58 +689,65 @@ def class1_ch6():
 	st.markdown("**:blue[Code]**")
 	with st.expander("Reveal Code"):
 		st.code('''
-		 def ch6():
-			st.title("Rule Based Bot")
+	  def ch6():
+		st.title("Rule Based Bot")
+	  
+	  	# Initialize chat history
+	  	if "messages" not in st.session_state:
+		st.session_state.messages = []
+	  
+	  	# Display chat messages from history on app rerun
+		for message in st.session_state.messages:
+			with st.chat_message(message["role"]):
+				st.markdown(message["content"])
+			
+		# React to user input
+		if prompt := st.chat_input("Enter your query"):
+			if prompt == "Hello":
+				with st.chat_message("user"):
+					st.write("Hello")
+					st.session_state.messages.append({"role": "user", "content": prompt})
+				with st.chat_message("assistant"):
+					reply = "Hi there what can I do for you"
+					st.write(reply)
+					st.session_state.messages.append(
+					{"role": "assistant", "content": reply}
+					)
 
-			# Initialize chat history
-			if "messages" not in st.session_state:
-				st.session_state.messages = []
+			elif prompt == "What is your name?":
+				with st.chat_message("user"):
+					st.write("What is your name?")
+					st.session_state.messages.append({"role": "user", "content": prompt})
+				with st.chat_message("assistant"):
+					reply = "My name is EAI , an electronic artificial being"
+					st.write(reply)
+					st.session_state.messages.append(
+					{"role": "assistant", "content": reply}
+					)
 
-			# Display chat messages from history on app rerun
-			# for message in st.session_state.messages:
-			# 	with st.chat_message(message["role"]):
-			# 		st.markdown(message["content"])
+			elif prompt == "How old are you?":
+				with st.chat_message("user"):
+					st.write("How old are you?")
+					st.session_state.messages.append({"role": "user", "content": prompt})
+				with st.chat_message("assistant"):
+					reply = "Today is my birthday!"
+					st.write(reply)
+					st.session_state.messages.append(
+					{"role": "assistant", "content": reply}
+					)
 
-			# React to user input
-			if prompt := st.chat_input("Enter your query"):
-				if prompt == "Hello":
-					with st.chat_message("user"):
-						st.write("Hello")
-						st.session_state.messages.append({"role": "user", "content": prompt})
-					with st.chat_message("assistant"):
-						reply = "Hi there what can I do for you"
-						st.write(reply)
-						st.session_state.messages.append({"role": "assistant", "content": reply})
-
-				elif prompt == "What is your name?":
-					with st.chat_message("user"):
-						st.write("What is your name?")
-						st.session_state.messages.append({"role": "user", "content": prompt})
-					with st.chat_message("assistant"):
-						reply = "My name is EAI , an electronic artificial being"
-						st.write(reply)
-						st.session_state.messages.append({"role": "assistant", "content": reply})
-
-				elif prompt == "How old are you?":
-					with st.chat_message("user"):
-						st.write("How old are you?")
-						st.session_state.messages.append({"role": "user", "content": prompt})
-					with st.chat_message("assistant"):
-						reply = "Today is my birthday!"
-						st.write(reply)
-						st.session_state.messages.append({"role": "assistant", "content": reply})
-
-				else:
-					with st.chat_message("user"):
-						st.write(prompt)
-						st.session_state.messages.append({"role": "user", "content": prompt})
-					with st.chat_message("assistant"):
-						reply = "I am sorry, I am unable to help you with your query"
-						st.write(reply)
-						st.session_state.messages.append({"role": "assistant", "content": reply})
-		
+			else:
+				with st.chat_message("user"):
+					st.write(prompt)
+					st.session_state.messages.append({"role": "user", "content": prompt})
+				with st.chat_message("assistant"):
+					reply = "I am sorry, I am unable to help you with your query"
+					st.write(reply)
+					st.session_state.messages.append(
+					{"role": "assistant", "content": reply}
+					)
 	  if __name__ == "__main__":
-	  	ch6()
+		ch6()
 		''')
 	st.markdown("**:red[Code Output]**")
 	st.title("Rule Based Bot")
