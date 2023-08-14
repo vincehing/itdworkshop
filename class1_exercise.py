42530a456b4bcbeb4c118d75cc7cb99d8b8bda98
lthben <Lthben@gmail.com>
2023-08-14 13:20:07 +0800

    c

--- class1_exercise.py
+++ class1_exercise.py
@@ -625,33 +625,33 @@ def class1_ex6():
 	st.write("You should see the output below when you run your programme.")
 	st.markdown("**:blue[Code]**")
 	st.code('''
-	 #Exercise 6 : Rule-based Echo Chatbot 
-	 def ex6():
-		st.title("Echo Bot")
+#Exercise 6 : Rule-based Echo Chatbot 
+def ex6():
+	st.title("Echo Bot")
 
-		# Initialize chat history
-		if "messages" not in st.session_state:
-			st.session_state.messages = []
+	# Initialize chat history
+	if "messages" not in st.session_state:
+		st.session_state.messages = []
 
-		# Display chat messages from history on app rerun
-		for message in st.session_state.messages:
-			with st.chat_message(message["role"]):
-				st.markdown(message["content"])
+	# Display chat messages from history on app rerun
+	for message in st.session_state.messages:
+		with st.chat_message(message["role"]):
+			st.markdown(message["content"])
 
-		# React to user input
-		if prompt := st.chat_input("What is up?"):
-			# Display user message in chat message container
-			st.chat_message("user").markdown(prompt)
-			# Add user message to chat history
-			st.session_state.messages.append({"role": "user", "content": prompt})
+	# React to user input
+	if prompt := st.chat_input("What is up?"):
+		# Display user message in chat message container
+		st.chat_message("user").markdown(prompt)
+		# Add user message to chat history
+		st.session_state.messages.append({"role": "user", "content": prompt})
 
-			response = f"Echo: {prompt}"
-			# Display assistant response in chat message container
-			with st.chat_message("assistant"):
-				st.markdown(response)
-			# Add assistant response to chat history
-			st.session_state.messages.append({"role": "assistant", "content": response})
-	 ''')
+		response = f"Echo: {prompt}"
+		# Display assistant response in chat message container
+		with st.chat_message("assistant"):
+			st.markdown(response)
+		# Add assistant response to chat history
+		st.session_state.messages.append({"role": "assistant", "content": response})
+''')
 	st.markdown("**:red[Code Output]**")
 	st.markdown("**Echo Bot**")
 
--- test.py
+++ test.py
@@ -1,18 +1,26 @@
-#Challenge 4 : Data and Loops
-def ch4():
-	name = st.text_input("Enter your name")
-	gender = st.selectbox("State your gender", ["Male", "Female"])
-	age = st.text_input("State your age", 18)
-	#declare empty dictionary
-	mydict = {}
-	mydict["name"] = name
-	mydict["gender"] = gender
-	mydict["age"] = age
-	#Print out the items in the dictionary
-	st.write("Here is your dictionary: ")
-	st.write(mydict)
+#Exercise 6 : Rule-based Echo Chatbot 
+ def ex6():
+	st.title("Echo Bot")
 
-	#show individual items in dictionary
-	st.write("You can also show individual items in the dictionary like this: ")
-	for key, value in mydict.items():
-		st.write(key + ": " + str(value))
+	# Initialize chat history
+	if "messages" not in st.session_state:
+		st.session_state.messages = []
+
+	# Display chat messages from history on app rerun
+	for message in st.session_state.messages:
+		with st.chat_message(message["role"]):
+			st.markdown(message["content"])
+
+	# React to user input
+	if prompt := st.chat_input("What is up?"):
+		# Display user message in chat message container
+		st.chat_message("user").markdown(prompt)
+		# Add user message to chat history
+		st.session_state.messages.append({"role": "user", "content": prompt})
+
+		response = f"Echo: {prompt}"
+		# Display assistant response in chat message container
+		with st.chat_message("assistant"):
+			st.markdown(response)
+		# Add assistant response to chat history
+		st.session_state.messages.append({"role": "assistant", "content": response})
