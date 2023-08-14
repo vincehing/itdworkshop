db2842b50b499ee44b528f0a7e02a6ff3d51c3d2
lthben <Lthben@gmail.com>
2023-08-14 14:31:47 +0800

    c

--- class1_exercise.py
+++ class1_exercise.py
@@ -1123,14 +1123,15 @@ def class1_ch10():
 	st.subheader("Challenge 10: Make your bot like someone you know!")
 	st.write("Now, let's create a variable to store a prompt to make your bot speak like someone you know! Get this prompt from the user.")
 	st.code('''
-	 prompt_template = st.chat_input("Enter a prompt to make your bot speak like someone you know!")
-	''')
-	st.write("You can use the code snippet below as a starter template. Note the ***prompt_template*** variable.")
+prompt_template := st.text_input("Enter a prompt to make your bot speak like someone you know!"):
+''')
+	st.write("You can use the code snippet below. Note the ***prompt_template*** variable.")
 	st.code('''
 	for response in openai.ChatCompletion.create(
 		model=st.session_state["openai_model"],
 		messages=[{"role":"system", "content":prompt_template}, {"role":"user", "content":prompt}],
 	''')
+	st.write("Try and get something similar to the code output below.")
 	st.write("Best prompt that gets the most votes win a prize!")
 	st.markdown("**:blue[Code]**")
 	with st.expander("Reveal Code"):
@@ -1149,41 +1150,46 @@ def ch10():
 	if "msg_bot" not in st.session_state:
 		st.session_state.msg_bot = []
 
-	for message in st.session_state.msg_bot:
-		with st.chat_message(message["role"]):
-			st.markdown(message["content"])
+	if prompt_template := st.text_input("Enter a prompt to make your bot speak like someone you know!"):
+		st.session_state.msg_bot.append({"role": "user", "content": prompt_template})
+		with st.chat_message("user"):
+			st.markdown(prompt_template)
+
+		with st.chat_message("assistant"):
+			full_response = "Nice! Now, let's test out your prompt template. Enter a prompt below to see how your bot responds."
+			st.markdown(full_response)
+		st.session_state.msg_bot.append({"role": "assistant", "content": full_response})
 	
-	try:
+		try:
 
-		if prompt := st.chat_input("What is up?"):
-			st.session_state.msg_bot.append({"role": "user", "content": prompt})
-			with st.chat_message("user"):
-				st.markdown(prompt)
+			if prompt := st.chat_input("What is up?"):
+				st.session_state.msg_bot.append({"role": "user", "content": prompt})
+				with st.chat_message("user"):
+					st.markdown(prompt)
 
-			with st.chat_message("assistant"):
-				message_placeholder = st.empty()
-				full_response = ""
-				for response in openai.ChatCompletion.create(
-					model=st.session_state["openai_model"],
-					messages=[
-								{"role": "system", "content": prompt_template},
-								{"role": "user", "content": prompt},
-							],
-					stream=True,
-				):
-					full_response += response.choices[0].delta.get("content", "")
-					message_placeholder.markdown(full_response + "▌")
-				message_placeholder.markdown(full_response)
-			st.session_state.msg_bot.append({"role": "assistant", "content": full_response})
+				with st.chat_message("assistant"):
+					message_placeholder = st.empty()
+					full_response = ""
+					for response in openai.ChatCompletion.create(
+						model=st.session_state["openai_model"],
+						messages=[
+									{"role": "system", "content": prompt_template},
+									{"role": "user", "content": prompt},
+								],
+						stream=True,
+					):
+						full_response += response.choices[0].delta.get("content", "")
+						message_placeholder.markdown(full_response + "▌")
+					message_placeholder.markdown(full_response)
+				st.session_state.msg_bot.append({"role": "assistant", "content": full_response})
 
-	except Exception as e:
-		st.error(e)
+		except Exception as e:
+			st.error(e)
 ''')
 	st.markdown("**:red[Code Output]**")
 	st.title("ChatGPT-like clone with Prompt Engineering")
 
 	openai.api_key = st.secrets["openapi_key"]
-
 
 	if "openai_model" not in st.session_state:
 		st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -1198,8 +1204,6 @@ def ch10():
 	# prompt_template = """
 	# 	Speak like Yoda from Star Wars for every question that was asked, do not give a direct answer but ask more questions in the style of wise Yoda from Star Wars
 	# """
-
-	# prompt_template = ""
 
 	if prompt_template := st.text_input("Enter a prompt to make your bot speak like someone you know!"):
 		st.session_state.msg_bot.append({"role": "user", "content": prompt_template})
