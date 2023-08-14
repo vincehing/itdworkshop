5ab24462539f16fd54ed0e9268278e498690e659
lthben <Lthben@gmail.com>
2023-08-14 14:18:26 +0800

    c

--- class1_exercise.py
+++ class1_exercise.py
@@ -1191,9 +1191,9 @@ def ch10():
 	if "msg_bot" not in st.session_state:
 		st.session_state.msg_bot = []
 
-	for message in st.session_state.msg_bot:
-		with st.chat_message(message["role"]):
-			st.markdown(message["content"])
+	# for message in st.session_state.msg_bot:
+	# 	with st.chat_message(message["role"]):
+	# 		st.markdown(message["content"])
 		
 	# prompt_template = """
 	# 	Speak like Yoda from Star Wars for every question that was asked, do not give a direct answer but ask more questions in the style of wise Yoda from Star Wars
